76ab25fa5f5b0349dc148c8dd47886e0d3942880
Laterner <ivantrofimov2001@gmail.com>
2021-03-17 12:24:16 +0300

    Initial commit

---
+++ Link.java
@@ -0,0 +1,19 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Link {
+    public URL url;
+    public int depth;
+
+    public Link(URL url, int depth) {
+        this.url = url;
+        this.depth = depth;
+    }
+
+    public Link(String url, int depth) throws MalformedURLException {
+        this.url = new URL(url);
+        this.depth = depth;
+    }
+
+
+}
